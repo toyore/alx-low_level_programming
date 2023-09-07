6f60ae6cc2f61af19dad8013392832351eb3755d
toyore <oluwatoyosioreniyi@gmail.com>
2023-09-07 10:40:53 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (SUCCESS)
+ */
+int main(void)
+{
+    /* Use printf to print the desired message with proper grammar */
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+
+    return (0);
+}
