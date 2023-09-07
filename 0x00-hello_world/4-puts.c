9778665333a6250e96648076c6ef7c946a35e237
toyore <oluwatoyosioreniyi@gmail.com>
2023-09-07 10:35:18 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- *main - main function
+ * main - main function
  *
  * Return: always 0 - success
  */
 int main(void)
 {
-	puts("/"The Programming is like building a multilingual puzzle");
+	puts("\"The Programming is like building a multilingual puzzle");
 	return (0);
 }
