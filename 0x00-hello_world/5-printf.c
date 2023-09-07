01a19f516bec325f0619318ade97428de4f157d9
toyore <oluwatoyosioreniyi@gmail.com>
2023-09-07 10:54:49 -0700

    pritf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-
 /**
- * main - Entry point of the program
+ * main - main function
  *
- * Return: Always 0 (SUCCESS)
+ * Return: always 0 - success
  */
 int main(void)
 {
     printf("with proper grammar, but the outcome is a piece of art,\n");
     return (0);
 }
-
