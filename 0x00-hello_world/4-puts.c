af162610327ab11f27ee6c5c4f45be0682f9883e
toyore <oluwatoyosioreniyi@gmail.com>
2023-09-07 09:32:44 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,19 @@
 #!/bin/bash
-echo '#include <stdio.h>' > prog.c && echo 'int main(void) { puts("Programming is like building a multilingual puzzle"); return 0; }' >> prog.c && gcc prog.c -o prog && ./prog && rm -f prog.c prog
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (SUCCESS)
+ */
+int main(void)
+{
+    char *message = "Programming is like building a multilingual puzzle\n";
+    ssize_t len = 47;  // Length of the message
+
+    /* Use write function to print the message to standard output (stdout) */
+    write(STDOUT_FILENO, message, len);
+
+    return (0);
+}
