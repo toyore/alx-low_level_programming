70e5b228ee1c30318c69335e11886aa100d4b2b7
toyore <oluwatoyosioreniyi@gmail.com>
2023-09-07 11:07:36 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
-nclude <stdio.h>
+#include <stdio.h>
 /**
- *  * main - main function
- *   *
- *    * Return: always 0 - success
- *     */
+ * main - main function
+ *   
+ * Return: always 0 - success
+ */
 int main(void)
 {
 	    printf("with proper grammar, but the outcome is a piece of art,\n");
-	        return (0);
+	    return (0);
 }
