12ed2f0daf2f81d4c53bfc726b4f78f29dced89d
toyore <oluwatoyosioreniyi@gmail.com>
2023-09-07 20:08:00 -0700

    m

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,6 +11,7 @@
 int main(void)
 {
         char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
         write(STDERR_FILENO, message, strlen(message));
         return (1);
 }
