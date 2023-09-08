d7d709649e88edd5ee207728e6300085ceabba69
toyore <oluwatoyosioreniyi@gmail.com>
2023-09-07 20:05:06 -0700

    m

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
-
 /**
- * main - Entry point
+ * main - main function
+ * 
+ * Description: Prints a specified message to standard error
  *
- * Description: Prints a specified message to standard error.
- *
- * Return: Always 1 (indicating an error).
+ * Return: always 1 (indicating an error)
  */
 int main(void)
 {
-    char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-    write(STDERR_FILENO, message, strlen(message));
-    return (1); // Add parentheses to the return statement
+        char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+        write(STDERR_FILENO, message, strlen(message));
+        return (1);
 }
