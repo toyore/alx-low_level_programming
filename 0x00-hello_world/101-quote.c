58580d0565c568d4a3c29f011caa8c2faf8a5185
toyore <oluwatoyosioreniyi@gmail.com>
2023-09-07 19:46:41 -0700

    error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+/**
+ *
+ * main - main function
+ * Description: Prints a specified message to standard error
+ * Return: always 1 (indicating an error)
+ */
+int main(void)
+{
+	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(STDERR_FILENO, message, strlen(message));
+	return 1;
+}
