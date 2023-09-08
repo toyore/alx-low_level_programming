808ca7cae25b16ae5456580127652e669fdb74ef
toyore <oluwatoyosioreniyi@gmail.com>
2023-09-08 12:09:58 -0700

    m

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,17 +12,19 @@ int main(void)
 {
 	int n;
 	srand(time(0));
+
 	n = rand() - RAND_MAX/2;
 	
 	printf("%d",n);
 	if (n > 0)
 	{
 		printf(" is positive\n");
-	}else if (n == 0)
+	}
+	if (n == 0)
 	{
 		printf(" is zero\n");
 	}
-	else
+	if (n < 0)
 	{
 		printf(" is negative\n");
 	}
