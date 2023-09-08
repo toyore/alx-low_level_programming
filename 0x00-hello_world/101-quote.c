761261a24911ad45b5eaf33aeededfef42919403
toyore <oluwatoyosioreniyi@gmail.com>
2023-09-07 19:55:00 -0700

    resolved git push

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+
 /**
  * main - main function
  * 
@@ -12,5 +13,5 @@ int main(void)
 {
 	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	write(STDERR_FILENO, message, strlen(message));
-	return 1;
+	return (1);
 }
