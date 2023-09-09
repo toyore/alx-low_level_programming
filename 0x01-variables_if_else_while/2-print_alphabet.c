64859dcd2a7bc432eaea1f2dbd1a3466b6dd93fa
toyore <oluwatoyosioreniyi@gmail.com>
2023-09-09 16:35:09 -0700

    print alphabeth

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Description: This is a program that prints alphabeth in lower cases
+ * Return: allows (0) success
+ */
+int main (void) //The entry point of our program
+{
+	char letter = 'a'; //start with 'a'
+	while (letter <= 'z') //iterate through 'a' to 'z'
+	{
+		putchar(letter); //print the current character and move to the next line
+		letter++; //increment and move to the next character
+	}
+	putchar ('\n'); //print a newline character
+	return (0);
+}
+
+
