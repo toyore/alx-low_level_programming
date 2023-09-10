2cc9097a15fd20d2ff339455575a823df3a47231
toyore <oluwatoyosioreniyi@gmail.com>
2023-09-10 11:00:36 -0700

    print alphabeth

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,13 +7,14 @@
 int main(void)
 {
 	char letter = 'a';
-
+	
 	while (letter <= 'z')
 	{
 		if (letter != 'q' && letter != 'e')
 		{
 			putchar(letter);
 		}
+		letter++;
 	}
 	putchar('\n');
 return (0);
