97b11e7cf93a67389a07eac7282641c0236a1f0b
toyore <oluwatoyosioreniyi@gmail.com>
2023-09-10 12:03:06 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Description: This is a program that prints digit in base 10
+ * Return: (0) success
+ */
+int main(void)
+{
+	int digit;
+	for (digit = 0; digit <= 9; digit++)
+	{
+		putchar(digit);
+		putchar('\n');
+	}
+return (0);
+}
