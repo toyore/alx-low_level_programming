065a2d1df6719928f7e469cc6e36d869459261f5
toyore <oluwatoyosioreniyi@gmail.com>
2023-09-10 11:28:44 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Description: This is a program that prints digits if base 10
+ * Return: allows (0) success
+ */
+int main(void)
+{
+	for (int digit = 0; digit <= 9; digit++)
+	{
+		printf("%d/n", digit);
+	}
+return (0);
+}
+
