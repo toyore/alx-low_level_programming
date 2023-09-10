845c38a0aaaf38615fe3ceddc0bc03fadfb9ee01
toyore <oluwatoyosioreniyi@gmail.com>
2023-09-10 12:40:49 -0700

    print

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Description: This is a program that prints letters in reverse
+ * Return: allows (0) success
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter <= 'a'; letter--)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+return (0);
+}
+
