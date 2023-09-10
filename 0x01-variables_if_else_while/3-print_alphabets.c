48b0cc377b1d5a018a605afe8bedd2660dbffd0d
toyore <oluwatoyosioreniyi@gmail.com>
2023-09-09 17:18:25 -0700

    print alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Description: This is a program that printsalphabeth in lower and upper case
+ * Return: allows (0) success
+ */
+int main(void)
+{
+	char smallLetter = 'a';
+
+		while (smallLetter <= 'z')
+		{
+			putchar(smallLetter);
+			smallLetter++;		}
+
+		char capitalLetter = 'A';
+
+			while (capitalLetter <= 'Z')
+			{
+				putchar(capitalLetter);
+				capitalLetter++;
+			}
+		putchar('\n');
+
+	return (0);
+}
