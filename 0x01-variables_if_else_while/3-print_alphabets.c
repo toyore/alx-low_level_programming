aeaf4c5e395e77d22598c2696714d4374e81e0c4
toyore <oluwatoyosioreniyi@gmail.com>
2023-09-10 03:56:22 -0700

    last digit

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,28 +1,25 @@
 #include <stdio.h>
 /**
  * main - main function
- * Description: This is a program that prints the alphabet in lowercase
- * and uppercase letters
- * Return: returns 0 on success
+ * Description: This is a program that prints alphabeth in lower cases
+ * Return: allows (0) success
  */
 int main(void)
 {
-    char letter = 'a';
-    char capLet = 'A';
+	char letter = 'a';
+        char capLet = 'A';
 
-    while (letter <= 'z')
-    {
-        putchar(letter);
-        letter++;
-    }
 
-    while (capLet <= 'Z')
-    {
-        putchar(capLet);
-        capLet++;
-    }
-
-    putchar('\n');
-    return (0);
-}
-  
+	while (letter <= 'z')
+	{
+		putchar(letter);
+                 letter++;
+	}
+	while (capLet <= 'Z')
+	{
+	       	putchar(capLet);
+                capLet++;
+        }
+	putchar ('\n');
+  return (0);
+  }
