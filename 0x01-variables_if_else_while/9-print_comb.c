c26f53de80ab29e68ac36a1e26021b08d292ab6b
toyore <oluwatoyosioreniyi@gmail.com>
2023-09-11 08:07:41 -0700

    i

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,12 +13,14 @@ int main(void)
        for (i = 0; i <= 9; i++)
        {
 	       putchar(i + '0');
-	       if (i != 9)
+	       if (i <= 9)
 	       {
 		       putchar(',');
 		       putchar(' ');
 	       }
+	       putchar('\n');
        }
-       putchar('\n');
 return (0);
 }
+       
+
