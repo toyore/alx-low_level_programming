76d9109690a2d6dfb15bddf44f19fd9b099992b3
toyore <oluwatoyosioreniyi@gmail.com>
2023-09-12 14:39:13 -0700

    nestes loop

---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - _putchar function
+ * Declaration: This is a program that print _putchar
+ * Return: allows ()) success
+ */
+int main(void)
+{
+       int _putchar(char c);
+        return (0);
+}
