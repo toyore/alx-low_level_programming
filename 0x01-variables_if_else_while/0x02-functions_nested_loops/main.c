5693c876eebb846bff3c0c5779a578fbff41a285
toyore <oluwatoyosioreniyi@gmail.com>
2023-09-12 13:43:52 -0700

    putchar

---
+++ 0x01-variables_if_else_while/0x02-functions_nested_loops/main.c
@@ -0,0 +1,9 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+/**
+ * main - _putchar function
+ * Description: This is a program tat prints _putchar
+ * Return: allows (0) success
+ */
+int putchar(char c);
+#endif
---
+++ 0x01-variables_if_else_while/0x02-functions_nested_loops/main.h
@@ -0,0 +1,9 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+/**
+ * main - _putchar function
+ * Description: This is a program that prints _putchar
+ * Return: allows (0) success
+ */
+int _putchar(char c);
+#endif
