ddb6de911b24b47cb1095d69b308d6674b9ca0c4
toyore <oluwatoyosioreniyi@gmail.com>
2023-09-13 00:34:04 -0700

    is alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,10 +1,14 @@
 #include "main.h"
 /**
- * _isalpha - checks if a character is alphabeth
- * @c: The character c checks
- * Description: This is a program that checks for alphabeth character
- * @c: The character to be checked
- * Return: 1 if c is a letter, lowercase or uppercase and 0 otherwise 
+ * _isalpha - checks if a character is an alphabet letter.
+ * @c: The character to be checked.
+ *
+ * Description:
+ * This function checks if the character 'c' is an alphabet letter,
+ * either lowercase or uppercase.
+ *
+ * Return:
+ * 1 if 'c' is a letter, lowercase or uppercase, and 0 otherwise.
  */
 int _isalpha(int c)
 {
