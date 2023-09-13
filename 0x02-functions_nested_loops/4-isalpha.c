bfb31f83d565973030c83012d34ef71e22b60a5e
toyore <oluwatoyosioreniyi@gmail.com>
2023-09-13 00:06:53 -0700

    is alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -7,12 +7,12 @@
  */
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z') && (c >= 'A' || c <= 'Z'))
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' || c <= 'Z'))
 	{
-		return(0);
+		return(1);
 	}
 	else
 	{
-		return (1);
+		return (0);
 	}
 }
