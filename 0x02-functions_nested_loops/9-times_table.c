a62076b1ea3b840d0ba05a1c7447f660716c8108
toyore <oluwatoyosioreniyi@gmail.com>
2023-09-13 04:15:32 -0700

    'times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,11 +16,11 @@ void times_table(void)
 
 			if (j == 0)
 			{
-				printf("%d", result);
+				printf("%2d", result);
 			}
 			else
 			{
-				printf(", %d", result);
+				printf(", %2d", result);
 			}
 		}
 		printf("\n");
