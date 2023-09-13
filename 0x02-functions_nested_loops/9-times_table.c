c25ccc93231528f09d7e4ceb1879a34c75cdaaae
toyore <oluwatoyosioreniyi@gmail.com>
2023-09-13 10:16:18 -0700

    m

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -20,8 +20,16 @@ void times_table(void)
 				if (result < 10)
 					_putchar(' ');
 			}
-			_putchar(result / 10 + '0');
-			_putchar(result % 10 + '0');
+			if (result >= 10)
+			{
+				_putchar(result / 10 + '0');
+				_putchar(result % 10 + '0');
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar(result + '0');
+			}
 		}
 		_putchar('\n');
 	}
