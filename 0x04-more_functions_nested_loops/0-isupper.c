6e5da89c75baeef1e4c7bed11e93db5c663baa83
toyore <oluwatoyosioreniyi@gmail.com>
2023-09-14 15:36:18 -0700

    upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -13,16 +13,12 @@ int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
 	{
-		int _isupper(int c)
-		{
-			if (c >= 'A' && c <= 'Z')
-			{
-				return (1);
-			}
-			else
-			{
-				return (0);
-			}
-		}
+		return (1);
+	}
+	else
+	{
+		return (0);
 	}
 }
+	
+
