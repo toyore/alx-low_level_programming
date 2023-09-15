6824dc44e7f9257ae7985486e7afde7201a209b1
toyore <oluwatoyosioreniyi@gmail.com>
2023-09-14 21:59:40 -0700

    m

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,6 +1,10 @@
 #include "main.h"
 /**
- * print numbers - Prints numbers from 0 to 9 followed by a new line
+ * print_numbers - Prints the digits from 0 to 9 followed by a new line.
+ *
+ * Description:
+ * This function prints the digits from 0 to 9 on a single line,
+ * followed by a new line character to create a newline.
  */
 void print_numbers(void)
 {
