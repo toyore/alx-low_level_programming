ca0ed9835f84374f75af32d0486a49be37d4dc99
toyore <oluwatoyosioreniyi@gmail.com>
2023-09-18 11:48:02 -0700

    print rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 /**
  * print_rev -  A function that prints a string, in reverse
  * , followed by a new line.
@@ -6,10 +7,24 @@
  */
 void print_rev(char *s)
 {
-	while (*s != '\0')
+	int length = 0;
+	int i;
+
+	if (s == NULL)
 	{
-		_putchar(*s);
-		s--;
+		return;
+	}
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+
+	i = length - 1;
+
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
 	}
 	_putchar('\n');
 }
