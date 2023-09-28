315eaf3717164cb4b58ab508e20e5d5a9af8666a
toyore <oluwatoyosioreniyi@gmail.com>
2023-09-28 13:12:36 -0700

    prime numbers

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,30 +1,48 @@
-#include <math.h>
+#include <stdio.h>
 
 /**
- * is_prime_number - Checks if an integer is a prime number.
- * @n: The integer to be checked.
+ * is_prime_number - Check if an integer is a prime number.
+ * @n: The integer to be checked for primality.
  *
- * Return: 1 if @n is a prime number, 0 otherwise.
+ * Description:
+ * This function checks whether the input integer 'n' is a prime number.
+ * A prime number is a positive integer greater than 1 that has no divisors
+ * other than 1 and itself.
+ *
+ * Return:
+ * 1 if 'n' is a prime number.
+ * 0 if 'n' is not a prime number.
  */
+int is_prime_number(int n);
+
 int is_prime_number(int n)
 {
 	int i;
 
-	/* Special cases: 0 and 1 are not prime numbers */
 	if (n <= 1)
 	{
-		return (0);
+		return (0); /* 0 and 1 are not prime numbers */
 	}
 
-	/* Check if n is divisible by any integer from 2 to sqrt(n) */
-	for (i = 2; i <= sqrt(n); i++)
+	if (n <= 3)
 	{
-		if (n % i == 0)
+		return (1); /* 2 and 3 are prime numbers */
+	}
+
+	if (n % 2 == 0 || n % 3 == 0)
+	{
+		return (0); /* Numbers divisible by 2 or 3 are not prime */
+	}
+
+	for (i = 5; i * i <= n; i += 6)
+	{
+		if (n % i == 0 || n % (i + 2) == 0)
 		{
-			return (0); /* n is divisible by i, so it's not prime */
+			return (0); /* Numbers divisible by i or i+2 are not prime */
 		}
 	}
 
-	return (1); /* n is a prime number */
+	return (1); /* If no divisors found, it's a prime number */
 }
+
 
