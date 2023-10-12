0c6c0b58b4a07349b24cecb4f61c70c6cc486c87
toyore <oluwatoyosioreniyi@gmail.com>
2023-10-12 00:39:15 -0700

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,50 @@
+#include <stdlib.h>
+#include <unistd.h>
+
+
+/**
+ * _strlen - Calculate the length of a string.
+ * @s: The string.
+ *
+ * Return: The length of the string.
+ */
+size_t _strlen(const char *s)
+{
+	size_t len = 0;
+
+	while (s[len])
+		len++;
+
+	return (len);
+}
+
+/**
+ * print_name - Print a dynamically allocated name to the standard output.
+ * @name: The name to print.
+ * @f: the pointer argument
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	size_t i;
+	size_t len = _strlen(name);
+	char *output = (char *)malloc(len + 1);
+
+	if (output == NULL)
+	{
+		write(2, "Memory allocation failed\n", 24);
+		/* 2 is the file descriptor for stderr */
+		exit(1);
+	}
+
+
+	for (i = 0; name[i]; i++)
+	{
+		output[i] = name[i];
+	}
+
+	output[len] = '\0';
+
+	f(output);
+
+	free(output);
+}
---
+++ 0x0F-function_pointers/main.h
@@ -0,0 +1,8 @@
+#ifndef PRINT_MAIN_H
+#define PRINT_MAIN_H
+
+#define PRINT_MAIN_H
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+
+#endif
