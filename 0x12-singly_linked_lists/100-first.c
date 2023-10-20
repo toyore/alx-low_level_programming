341ffc6c1446efdec0175f69f53334e0a825519c
toyore <oluwatoyosioreniyi@gmail.com>
2023-10-20 01:19:10 -0700

    bettty

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,7 +5,7 @@
  */
 void print_before_main(void)
 {
-	printf("You're beat! and yet, you must allow,
-			\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
 
