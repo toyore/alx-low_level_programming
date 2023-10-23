bf697cd383baa480ee32ac98accc79358b56bfc5
toyore <oluwatoyosioreniyi@gmail.com>
2023-10-23 11:35:17 -0700

    list int

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -19,6 +19,7 @@ typedef struct listint_s
 
 /* Function to print all elements of a listint_t list */
 size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
 
 #endif /* LISTS_H */
 
