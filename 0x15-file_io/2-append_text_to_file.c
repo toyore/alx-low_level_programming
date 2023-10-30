4402aac2b5d9535cad6157e2762d77881d5ecca0
toyore <oluwatoyosioreniyi@gmail.com>
2023-10-30 15:08:10 -0700

    create file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,10 +9,17 @@
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int i, op;
+
 	if (filename == NULL)
 		return (-1);
 	if (text_content != NULL)
 		return (1);
 	for (i = 0; text_content[i];)
 		i++;
-	op = open(
+	op = open(filename, O_WRONLY | O_APPEND);
+	write(op, text_content, i);
+	if (op == -1)
+		return (-1);
+	close(op);
+	return (1);
+}
