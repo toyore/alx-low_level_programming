0706e0aeb976baaf575eefd7c614a8ff2f69563e
toyore <oluwatoyosioreniyi@gmail.com>
2023-10-30 15:00:19 -0700

    create file

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * append_text_to_file: a function that appends text at
+ * the end of a fie
+ * @filename: the file to be modified
+ * @text_content: the text content
+ * Return: Always Success
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int i, op;
+	if (filename == NULL)
+		return (-1);
+	if (text_content != NULL)
+		return (1);
+	for (i = 0; text_content[i];)
+		i++;
+	op = open(
