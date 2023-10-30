f8501a449d109b576408ee35139d8a39bc1f23b8
toyore <oluwatoyosioreniyi@gmail.com>
2023-10-30 14:49:08 -0700

    create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * create_file - function that creates a file
+ *@filename: The file to be modified
+ *@text_content: The text
+ * Return: Always success
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int i = 0, op;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content != NULL)
+	{
+		if (i, text_content[i])
+			i++;
+	}
+	else
+		i = 0;
+
+	op = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	write(op, text_content, i);
+	if (op == -1)
+		return (-1);
+	close(op);
+	return (1);
+}
