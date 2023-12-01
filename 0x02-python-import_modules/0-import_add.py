2e018f7c874643199b3df7811c68101d93d4c452
toyore <oluwatoyosioreniyi@gmail.com>
2023-11-30 17:15:52 -0800

    add

--- 0x02-python-import_modules/0-import_add.py
+++ 0x02-python-import_modules/0-import_add.py
@@ -1,7 +1,8 @@
+#!/user/bin/python3
+from add_0.py import add
 a = 1;
 b = 2;
 
-0-add = __import__("0-add");
-result = 0-add.add(a, b);
+result = add(a, b);
 
-print(f'{a} + {b} = {resuts}')
+print(f'{a} + {b} = {result}')
